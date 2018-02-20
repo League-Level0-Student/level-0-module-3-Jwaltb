bc978aa2b6c06a8a9cc5235b38fad4b1e928cdb5
jbear18 <league@worthymini-5.attlocal.net>
2018-02-19 19:54:21 -0800

    HighLowGame RobotColorChooser

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,32 +5,45 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
-		
+		// 3. Change this line to give you a random number between 1 - 100.
+		int random = new Random().nextInt((100 - 1) + 1) + 1;
+
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
-		
-			// 1. Ask the user for a guess using a pop-up window, and save their response 
+		for (int i = 0; i < 11; i++) {
 
+			// 1. Ask the user for a guess using a pop-up window, and save their response
+			String guess = JOptionPane.showInputDialog("Pick a number between 1 and 100");
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int number = Integer.parseInt(guess);
 			// 5. if the guess is correct
-				// 6. Win
-				// 11. Use "System.exit(0);" to quit the game if the user guessed the right answer.
+			// 6. Win
+			if (number == random) {
+				JOptionPane.showMessageDialog(null, "You win!");
+				System.exit(0);
+			}
+			// 11. Use "System.exit(0);" to quit the game if the user guessed the right
+			// answer.
+			else if (number > random) {
+				JOptionPane.showMessageDialog(null, "Your guess is to high");
+			}
 			// 7. if the guess is high
-				// 8. Tell them it's too high
+			// 8. Tell them it's too high
 			// 9. if the guess is low
-				// 10. Tell them it's too low
-
+			else {
+				JOptionPane.showMessageDialog(null, "Your guess is too low");
+			}
+			// 10. Tell them it's too low
+		}
 		// 12. Tell them they lose
-		
+		JOptionPane.showMessageDialog(null, "You lose");
+
 	}
 
 }
-
-
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,47 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//4. Use an if/else statement to set the pen color that the user requested
+		// 1. Create a new Robot
+		Robot rob = new Robot();
+		{
 
-        //5. If the user doesn’t enter anything, choose a random color
+			rob.setSpeed(25);
+			rob.penDown();
+			for (int i = 0; i < 4; i++) {
 
-        //6. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
+				// 3. Ask the user what color they would like the robot to draw
+				String color = JOptionPane.showInputDialog("Choose a color");
+				// 4. Use an if/else statement to set the pen color that the user requested
+				if (color.equalsIgnoreCase("red")) {
+					rob.setPenColor(Color.red);
+				} else if (color.equalsIgnoreCase("blue")) {
+					rob.setPenColor(Color.blue);
+				} else if (color.equalsIgnoreCase("green")) {
+					rob.setPenColor(Color.green);
+				} else {
+					rob.setRandomPenColor();
+				}
+				// 5. If the user doesn’t enter anything, choose a random color
 
+				// 6. Put a loop around your code so that you keep asking the user for more
+				// colors & drawing them
 
+				// 4. Set the pen width to 10
+				rob.setPenWidth(10);
+				// 2. Make the robot draw a shape (this will take more than one line of code)
+				for (int x = 0; x < 4; x++) {
+
+					rob.move(150);
+					rob.turn(360 / 4);
+				}
+			}
+		}
 	}
 }
